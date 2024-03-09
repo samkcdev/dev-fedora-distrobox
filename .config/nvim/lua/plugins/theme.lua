fe910467037b7d7c868d9bd04378c542f5ddb18e
samkcdev <samnp87@gmail.com>
2024-03-09 15:55:35 +0530

    lazyvim installed

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,20 @@
-
-return{
-  'projekt0n/github-nvim-theme',
-  lazy = false,
-  priority = 1000,
-  config = function()
-   vim.cmd.colorscheme ("github_dark_default")
-  end,
+return {
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_high_contrast",
+    },
+  },
 }
